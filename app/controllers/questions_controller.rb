27570f6c5c03fa4f0a99dac59febe500880d425e
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-24 00:39:03 +0900

    Add: 質問管理機能一通り実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,49 @@
 class QuestionsController < ApplicationController
+  before_action :set_question,only: %i(show edit update destroy)
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to question_path(@question.id),notice:"質問を投稿しました！"
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_path)
+      redirect_to question_path(@question.id),notice:"質問を更新しました！"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @question.destroy
+    if @question.destroy
+      redirect_to questions_path,notice:"質問を削除しました！"
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title,:details,:status)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :questions,foreign_key:"questioner_id",dependent: :destroy
+
   before_validation { email.downcase! }
   has_secure_password
   validates :name,presence: true,length: {maximum: 30 }
