0559230d6c10ad933d25e0d7e68b6e72c1742367
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-25 05:06:21 +0900

    Add: answerのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
   resources :sessions,only:[:new,:create,:destroy]
 end
