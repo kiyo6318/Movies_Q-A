d23152d4d0bdaea26e9d70c6a1097d4c32092fb2
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-24 23:01:13 +0900

    Add: テスト追加

---
+++ spec/questions_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.feature "質問管理機能",type: :system do
+  background do
+    @test_questioner = User.create(name:"テストユーザー",email:"test@mail.com",password:"password",password_confirmation:"password")
+    @test_question = Question.create(title:"テストタイトル",details:"テスト詳細",status:0,questioner_id:@test_questioner.id)
+    visit new_session_path
+    fill_in "Email",with: "test@mail.com"
+    fill_in "Password",with: "password"
+    click_button "Log in"
+  end
+  scenario "質問投稿テスト" do
+    visit new_question_path
+    fill_in "Title",with: "投稿テストタイトル"
+    fill_in "Details",with: "投稿テスト詳細"
+    click_button "登録する"
+    expect(page).to have_content "投稿テストタイトル"
+  end
+
+  scenario "質問一覧テスト" do
+    Question.create(title:"テストタイトル2",details:"テスト詳細2",status:0,questioner_id:@test_questioner.id)
+    visit questions_path
+    expect(page).to have_content "テストタイトル"
+    expect(page).to have_content "テストタイトル2"
+  end
+
+  scenario "質問詳細テスト" do
+    visit questions_path
+    click_link "テストタイトル"
+    expect(page).to have_content "テストユーザー"
+    expect(page).to have_content "テストタイトル"
+    expect(page).to have_content "テスト詳細"
+    expect(page).to have_content "未解決"
+  end
+
+  scenario "質問編集テスト" do
+    visit questions_path
+    click_link "テストタイトル"
+    click_link "質問編集"
+    fill_in "Details",with: "編集テスト詳細"
+    click_button "更新する"
+    expect(page).to have_content "編集テスト詳細"
+  end
+
+  scenario "質問削除テスト" do
+    visit questions_path
+    click_link "テストタイトル"
+    click_link "質問編集"
+    click_link "質問削除"
+    expect(page.driver.browser.switch_to.alert.text).to have_content "本当に削除しますか？"
+    page.driver.browser.switch_to.alert.accept
+    expect(page).not_to have_content "テストタイトル"
+  end
+end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "ユーザー管理機能",type: :system do
   background do
-    @user = User.create(name:"テスト",email:"test@mail.com",admin: false,password:"password",password_confirmation:"password")
+    @user = User.create(name:"テストユーザー",email:"test@mail.com",admin: false,password:"password",password_confirmation:"password")
+    visit new_session_path
+    fill_in "Email",with: "test@mail.com"
+    fill_in "Password",with: "password"
+    click_button "Log in"
   end
   scenario "ユーザー作成テスト" do
     visit new_user_path
@@ -10,27 +14,29 @@ RSpec.feature "ユーザー管理機能",type: :system do
     fill_in "Email",with: "create_test@mail.com"
     fill_in "Password",with: "password"
     fill_in "Password confirmation",with: "password"
-    click_button "Create User"
+    click_button "登録する"
     expect(page).to have_content "作成テスト"
   end
 
   scenario "ユーザー 一覧テスト" do
+    User.create(name:"ユーザー 一覧テスト",email:"indextest@mail.com",admin: false,password:"password",password_confirmation:"password")
     visit users_path
-    expect(page).to have_content "テスト"
+    expect(page).to have_content "ユーザー 一覧テスト"
+    expect(page).to have_content "テストユーザー"
   end
 
   scenario "ユーザー詳細テスト" do
     visit user_path(@user.id)
-    expect(page).to have_content "テスト"
+    expect(page).to have_content "テストユーザー"
   end
 
   scenario "ユーザー編集テスト" do
     visit edit_user_path(@user.id)
-    fill_in "Name",with: "編集テスト"
+    fill_in "Name",with: "ユーザー編集テスト"
     fill_in "Password",with: "password"
     fill_in "Password confirmation",with: "password"
-    click_button "Update User"
-    expect(page).to have_content "編集テスト"
+    click_button "更新する"
+    expect(page).to have_content "ユーザー編集テスト"
   end
 
   scenario "ユーザー削除テスト" do
@@ -39,5 +45,21 @@ RSpec.feature "ユーザー管理機能",type: :system do
     expect(page.driver.browser.switch_to.alert.text).to have_content "本当に削除しますか？"
     page.driver.browser.switch_to.alert.accept
     expect(page).not_to have_content "テスト"
+  end
+
+  scenario "ログイン&ログアウト" do
+    click_link "ログアウト"
+    click_link "ログイン"
+    fill_in "Email",with: "test@mail.com"
+    fill_in "Password",with: "password"
+    click_button "Log in"
+    expect(page).to have_content "ユーザープロフィール"
+    expect(page).to have_content "テストユーザー"
+  end
+
+  scenario "ログインしないと質問投稿できないテスト" do
+    click_link "ログアウト"
+    visit new_question_path
+    expect(page).to have_content "ログインが必要です"
   end
 end
