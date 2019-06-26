48215a14d1968480b4cbfa36cd91d8941946b1dc
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-26 17:57:53 +0900

    Add: labelモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_200326) do
+ActiveRecord::Schema.define(version: 2019_06_26_085201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2019_06_24_200326) do
     t.datetime "updated_at", null: false
     t.index ["answerer_id"], name: "index_answers_on_answerer_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
