88a8b4ea672aac09367faabf5d07a6569d1d8cf8
前川清成 <kiyonari6318@MacbookPro.local>
2019-06-26 16:02:03 +0900

    Add: 回答機能一通り実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,6 +21,8 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @answers = @question.answers
+    @answer = @question.answers.build
   end
 
   def edit
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user,foreign_key: "questioner_id"
+  has_many :answers,dependent: :destroy
   validates :title,presence: true
   validates :details,presence: true
   enum status:{unsolved:0,resolved:1}
