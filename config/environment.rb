d4d69e79899521ec8ac0d23ff17cf965a22a6864
前川清成 <kiyonari6318@MacbookPro.local>
2019-08-17 12:44:00 +0900

    Fix: environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
-# Load the Rails application.
-require_relative 'application'
+# # Load the Rails application.
+# require_relative 'application'
+
+# # Initialize the Rails application.
+# Rails.application.initialize!
+
+require File.expand_path('../application', __FILE__)
+# Initialize the Rails application.
+Rails.application.initialize!# Load the Rails application.
 
 # Initialize the Rails application.
-Rails.application.initialize!
+#Rails.application.initialize!
