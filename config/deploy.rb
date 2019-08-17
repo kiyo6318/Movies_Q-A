e3df01162d72a6a6d345cd1f33c1b1c6dca3b132
前川清成 <kiyonari6318@MacbookPro.local>
2019-08-17 19:09:20 +0900

    Fix: deploy.rb修正6

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,12 +43,23 @@ namespace :deploy do
     end
   end
 
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
+  # desc 'Run seed'
+  # task :seed do
+  #   on roles(:app) do
+  #     with rails_env: fetch(:rails_env) do
+  #       within current_path do
+  #         execute :bundle, :exec, :rake, 'db:seed'
+  #       end
+  #     end
+  #   end
+  # end
+
+  desc 'seed_fu'
+  task :seed_fu do
+    on roles(:db) do |host|
       with rails_env: fetch(:rails_env) do
         within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
+          execute :bundle, :exec, :rails, 'db:seed_fu'
         end
       end
     end
@@ -60,13 +71,6 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
-
-  task :apply_seedfu do
-    on primary :db do
-      invoke 'seed_fu:apply'
-    end
-  end
-  after 'deploy:migrating', 'deploy:apply_seedfu'
 
   # desc 'Load seed data into database'
   # task :seed_fu do
